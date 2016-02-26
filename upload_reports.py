89afe4e54a9a9233dd0519c895a85367455bb66c
migbro <miguel.a.brown@gmail.com>
2016-02-26 12:06:28 -0600

    Figured out authentication and csrf token passing.

--- upload_reports.py
+++ upload_reports.py
@@ -2,16 +2,17 @@
 """
 Plugin to interface with variant viewer - upload reports after pipeline is run.  Currently assume pairs...
 
-Usage: ./upload_reports.py <list> <urlGet> <urlUp> <genome> <caller>
+Usage: ./upload_reports.py <list> <config>
 
 Arguments:
     <list>  list of variant reports to upload
-    <urlGet>   url to query
-    <urlUp>    url to upload to
+    <config>    json config file with genome, caller, and get/post urls
 
 Options:
     -h
 """
+
+import json
 import os
 import pdb
 import re
@@ -21,13 +22,14 @@ import requests
 from docopt import docopt
 
 args = docopt(__doc__)
-get_url = args['<urlGet>']
 
 
-def get_info(bid, caller, genome, url):
-    # perhaps switch post of a json object with genome and caller as well?
+def get_info(bid, url):
     info_req = {'bid': bid, 'caller': caller, 'genome': genome}
-    return requests.post(url, json=info_req)
+    # cookies = dict(get_client.cookies)
+    headers = {'Content-type': 'application/json'}  # ,  "X-CSRFToken": get_csrftoken}
+    return requests.post(url, data=json.dumps(info_req), headers=headers)
+    # return get_client.post(url, data=json.dumps(info_req), headers=headers, cookies=cookies)
 
 
 def create_report_name(sample1, sample2, bid1, bid2):
@@ -66,13 +68,33 @@ def create_report_name(sample1, sample2, bid1, bid2):
         rname += bid2 + ')'
     return rname
 
+def set_web_stuff(client, url):
+    client.get(url)
+    return client.cookies['csrftoken'], dict(post_client.cookies), {"X-CSRFToken": client.cookies['csrftoken'], "Referer": login_url}
+
+# get config variables
+config_data = json.loads(open(args.get('<config>'), 'r').read())
+(login_url, get_url, post_url, genome, caller, username, password) = (
+    config_data['login_url'], config_data['urlGet'], config_data['urlUp'], config_data['genome'], config_data['caller'],
+    config_data['username'], config_data['password'])
+
+# get token to gain access to site
+# get_client = requests.session()
+# get_client.get(login_url)
+# get_csrftoken = get_client.cookies['csrftoken']
+
+post_client = requests.session()
+(post_csrftoken, post_cookies, post_headers) = set_web_stuff(post_client, login_url)
+login_data = dict(username=username, password=password)
+r = post_client.post(login_url, login_data, cookies=post_cookies, headers=post_headers)
 
 for fn in open(args['<list>'], 'r'):
     fn = fn.rstrip('\n')
     bnids = re.match('^(\d+-\d+)_(\d+-\d+)', os.path.basename(fn))
     (bid1, bid2) = (bnids.group(1), bnids.group(2))
-    sample1_obj = get_info(bid=bid1, url=get_url, genome=args.get('<genome>'), caller=args.get('<caller>'))
-    sample2_obj = get_info(bid=bid2, url=get_url, genome=args.get('<genome>'), caller=args.get('<caller>'))
+
+    sample1_obj = get_info(bid=bid1, url=get_url)
+    sample2_obj = get_info(bid=bid2, url=get_url)
     (bid1_pk, bid2_pk) = (sample1_obj.json()['bid_pk'], sample2_obj.json()['bid_pk'])
     study_pk = sample1_obj.json()['study']
     genome_pk = sample1_obj.json()['genome_pk']
@@ -81,11 +103,11 @@ for fn in open(args['<list>'], 'r'):
     metadata = {'name': [report_name], 'study': [study_pk], 'bnids': (bid1_pk, bid2_pk), 'genome': [genome_pk],
                 'caller': [caller_pk]}
     files = {'report_file': (fn, open(fn, 'rb'), 'application/octet-stream')}
-
     pdb.set_trace()
-    # metadata = 'file=' + fn + ';name=' + report_name + ';study=' + study_pk + ';genome=' + gen
-    # issuing a curl command may be easier to emulate a form submission
-    check = requests.post(args.get('<urlUp>'), data=metadata, files=files)
+    (post_csrftoken, post_cookies, post_headers) = set_web_stuff(post_client, login_url)
+    check = post_client.post(post_url, data=metadata, files=files, headers=post_headers, cookies=post_cookies,
+                             allow_redirects=False)
+    # pdb.set_trace()
     slow_down = 'whoa horse!'
 
 
