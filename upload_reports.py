592678c7f5b455eadfe45cb039e960e02bdee0e8
migbro <miguel.a.brown@gmail.com>
2016-02-26 12:21:08 -0600

    Cleaned up code a bit.

--- upload_reports.py
+++ upload_reports.py
@@ -14,17 +14,14 @@ Options:
 
 import json
 import os
-import pdb
 import re
 import sys
 
 import requests
 from docopt import docopt
 
-args = docopt(__doc__)
 
-
-def get_info(bid, url):
+def get_info(bid, url, caller, genome):
     info_req = {'bid': bid, 'caller': caller, 'genome': genome}
     # cookies = dict(get_client.cookies)
     headers = {'Content-type': 'application/json'}  # ,  "X-CSRFToken": get_csrftoken}
@@ -68,48 +65,55 @@ def create_report_name(sample1, sample2, bid1, bid2):
         rname += bid2 + ')'
     return rname
 
+
 def set_web_stuff(client, url):
     client.get(url)
-    return client.cookies['csrftoken'], dict(post_client.cookies), {"X-CSRFToken": client.cookies['csrftoken'], "Referer": login_url}
+    return client.cookies['csrftoken'], dict(client.cookies), {"X-CSRFToken": client.cookies['csrftoken'],
+                                                               "Referer": url}
+
 
 # get config variables
-config_data = json.loads(open(args.get('<config>'), 'r').read())
-(login_url, get_url, post_url, genome, caller, username, password) = (
-    config_data['login_url'], config_data['urlGet'], config_data['urlUp'], config_data['genome'], config_data['caller'],
-    config_data['username'], config_data['password'])
+def upload_reports():
+    args = docopt(__doc__)
+    config_data = json.loads(open(args.get('<config>'), 'r').read())
+    (login_url, get_url, post_url, genome, caller, username, password) = (
+        config_data['login_url'], config_data['urlGet'], config_data['urlUp'], config_data['genome'],
+        config_data['caller'],
+        config_data['username'], config_data['password'])
 
-# get token to gain access to site
-# get_client = requests.session()
-# get_client.get(login_url)
-# get_csrftoken = get_client.cookies['csrftoken']
+    # get token to gain access to site
+    # get_client = requests.session()
+    # get_client.get(login_url)
+    # get_csrftoken = get_client.cookies['csrftoken']
 
-post_client = requests.session()
-(post_csrftoken, post_cookies, post_headers) = set_web_stuff(post_client, login_url)
-login_data = dict(username=username, password=password)
-r = post_client.post(login_url, login_data, cookies=post_cookies, headers=post_headers)
-
-for fn in open(args['<list>'], 'r'):
-    fn = fn.rstrip('\n')
-    bnids = re.match('^(\d+-\d+)_(\d+-\d+)', os.path.basename(fn))
-    (bid1, bid2) = (bnids.group(1), bnids.group(2))
-
-    sample1_obj = get_info(bid=bid1, url=get_url)
-    sample2_obj = get_info(bid=bid2, url=get_url)
-    (bid1_pk, bid2_pk) = (sample1_obj.json()['bid_pk'], sample2_obj.json()['bid_pk'])
-    study_pk = sample1_obj.json()['study']
-    genome_pk = sample1_obj.json()['genome_pk']
-    caller_pk = sample1_obj.json()['caller_pk']
-    report_name = create_report_name(sample1=sample1_obj, sample2=sample2_obj, bid1=bid1, bid2=bid2)
-    metadata = {'name': [report_name], 'study': [study_pk], 'bnids': (bid1_pk, bid2_pk), 'genome': [genome_pk],
-                'caller': [caller_pk]}
-    files = {'report_file': (fn, open(fn, 'rb'), 'application/octet-stream')}
-    pdb.set_trace()
+    post_client = requests.session()
     (post_csrftoken, post_cookies, post_headers) = set_web_stuff(post_client, login_url)
-    check = post_client.post(post_url, data=metadata, files=files, headers=post_headers, cookies=post_cookies,
-                             allow_redirects=False)
-    # pdb.set_trace()
-    slow_down = 'whoa horse!'
+    login_data = dict(username=username, password=password)
+    r = post_client.post(login_url, login_data, cookies=post_cookies, headers=post_headers)
+
+    for fn in open(args['<list>'], 'r'):
+        fn = fn.rstrip('\n')
+        bnids = re.match('^(\d+-\d+)_(\d+-\d+)', os.path.basename(fn))
+        (bid1, bid2) = (bnids.group(1), bnids.group(2))
+
+        sample1_obj = get_info(bid=bid1, url=get_url, caller=caller, genome=genome)
+        sample2_obj = get_info(bid=bid2, url=get_url, caller=caller, genome=genome)
+        (bid1_pk, bid2_pk) = (sample1_obj.json()['bid_pk'], sample2_obj.json()['bid_pk'])
+        study_pk = sample1_obj.json()['study']
+        genome_pk = sample1_obj.json()['genome_pk']
+        caller_pk = sample1_obj.json()['caller_pk']
+        report_name = create_report_name(sample1=sample1_obj, sample2=sample2_obj, bid1=bid1, bid2=bid2)
+        metadata = {'name': [report_name], 'study': [study_pk], 'bnids': (bid1_pk, bid2_pk), 'genome': [genome_pk],
+                    'caller': [caller_pk]}
+        files = {'report_file': (fn, open(fn, 'rb'), 'application/octet-stream')}
+        (post_csrftoken, post_cookies, post_headers) = set_web_stuff(post_client, login_url)
+        post_client.post(post_url, data=metadata, files=files, headers=post_headers, cookies=post_cookies,
+                         allow_redirects=False)
 
 
-# TO DO - parse for tumor normal, generate report name
-# upload using requests.put and requests.file into upload_report method of viewer
+def main():
+    upload_reports()
+
+
+if __name__ == '__main__':
+    main()
